fc03f43fcfe4793d283c9f94d5b10b20e5a66b1d
Kamil <kamiluk@onet.pl>
2024-01-23 11:24:36 +0100

    Pierwszy egzamin i 3/4 drugiego haha

---
+++ Egzamin2/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+namespace Egzamin2
+{
+    class Program
+    {
+        /* Treść
+            Wstęp Do Programowania – Egzamin Praktyczny 2022.01.14
+        Zestaw A
+        Piszę z pamięci treści zadań (prócz czwartego), na podstawie plików, które wstawiłem na moodle po egzaminie.
+
+        Zadanie 1
+        Stwórz funkcję static ulong A1(uint[] T), która zwraca sumę liczb pierwszych z tablicy T. Jeśli w tablicy nie ma liczb pierwszych, funkcja zwraca 1.
+        Przykład: Dla tablicy {2}, funkcja zwraca 18, bo (7+11) = 18
+        6
+
+        Zadanie 2
+        Stwórz funkcję static uint A2(int liczba), która w sposób rekurencyjny zwraca najbardziej znaczącą cyfrę zapisu siódemkowego cyfry dziesiętnej przekazanej jako parametr.
+        Przykład: liczba -123 w zapisie siódemkowym to -234. Funkcja zwraca 2.
+
+        Zadanie 3
+        Stwórz funkcję static uint A3(string napis), która zwraca liczbę wyrazów, które spełniają warunek, że liczba spółgłosek w wyrazie stanowi co najmniej połowę liter wyrazu. Wyrazy w napisie mogą być oddzielone dowolnymi separatorami (kropka, spacja, wielokropek, średnik, itp.). Napis nie zawiera liter z polskimi ogonkami (ą, ę, itp.). 
+        Przykład: Dla napisu „Mama, tata i Ala!”, funkcja zwraca 2.
+
+        Zadanie 4
+        Stwórz funkcję static ulong A4(ulong dane), która zmodyfikuje w otrzymanym ciągu 64. bitów wartość przechowywaną na 4. bitach: od 7 do 10 bitu. Funkcja zwiększa tę wartość o 1. Jeśli przechowywana wartość to 1111, zwiększona wartość, to 0000. Pozostałych bitów nie wolno zmodyfikować.
+        */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(A1(new uint[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })); // 2 + 3 + 5 + 7 = 17
+            Console.WriteLine(A2(-23133123)); // 4
+            Console.WriteLine(A3("Mama, tata i Ala!")); // 2
+
+            Console.ReadKey();
+        }
+
+        #region Zadanie 1
+
+        static bool CzyPierwsza(uint liczba)
+        {
+            if (liczba < 2)
+                return false;
+
+            for (uint i = 2; i < liczba; i += 1)
+                if (liczba % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        static ulong A1(uint[] T)
+        {
+            ulong suma = 0;
+
+            foreach (uint liczba in T)
+            {
+                if (CzyPierwsza(liczba))
+                    suma += liczba;
+            }
+
+            return suma;
+        }
+
+        #endregion
+
+        #region Zadanie 2
+
+        // ZADANIE 2
+        static uint A2(int liczba) // Na podstawie Przykład #1 - Wyświetl binarnie i https://www.youtube.com/watch?v=VUHwfugYFEA
+        {
+            if (liczba < 0)
+                return A2(-liczba / 7);
+
+            if (liczba < 7)
+                return (uint) liczba;
+
+            return A2(liczba / 7);
+        }
+
+
+        #endregion
+
+        #region Zadanie 3
+
+        static bool CzyLitera(char znak)
+        {
+            if (znak >= 'a' && znak <= 'z')
+                return true;
+
+            if (znak >= 'A' && znak <= 'Z')
+                return true;
+
+            return false;
+        }
+
+        static bool CzySamogłoska(char znak)
+        {
+            char[] samogłoski = {'a', 'ą', 'e', 'ę', 'i', 'o', 'u', 'y',
+                                 'A', 'Ą', 'E', 'Ę', 'I', 'O', 'U', 'Y'};
+
+
+            foreach (char samogłoska in samogłoski)
+                if (samogłoska == znak)
+                    return true;
+
+            return false;
+        }
+
+        static uint A3(string napis)
+        {
+            uint wyrazySpelniajaceWarunek = 0;
+            uint liczbaSamogłosek = 0;
+            uint liczbaSpółgłosek = 0;
+            
+            foreach (char znak in napis)
+            {
+                if (CzySamogłoska(znak))
+                {
+                    liczbaSamogłosek += 1;
+                    continue;
+                }
+                
+                if (CzyLitera(znak))
+                {
+                    liczbaSpółgłosek += 1;
+                    continue;
+                }
+
+                if (liczbaSpółgłosek != 0 && liczbaSpółgłosek >= liczbaSamogłosek) // Jesli cos nie jest wyrazem to liczba spólglosek i samogłosek bedzie rowna 0, trzeba brac to pod uwage
+                    wyrazySpelniajaceWarunek += 1;
+
+                liczbaSamogłosek = 0;
+                liczbaSpółgłosek = 0;
+            }
+
+            return wyrazySpelniajaceWarunek;
+        }
+
+        #endregion
+
+        #region Zadanie 4
+
+        static string ZwrocBinarnie(ulong liczba) // Funkcja pomocnicza
+        {
+            string wynik = "";
+
+            while (liczba > 0)
+            {
+                wynik = liczba % 2 + wynik;
+                liczba = liczba / 2;
+            }
+
+            return wynik;
+        }
+
+        static ulong A4(ulong dane)
+        {
+            var minusPierwszych6Bitow = dane >> 6;
+            if ((minusPierwszych6Bitow & 0b1111) == 0b1111)
+            {
+                Console.WriteLine("Zamiana na 0000");
+            }
+            Console.WriteLine(ZwrocBinarnie(dane));
+            Console.WriteLine(ZwrocBinarnie(minusPierwszych6Bitow));
+            return 0;
+        }
+        
+        #endregion
+
+    }
+}
---
+++ WDP Egzamin/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.IO;
+
+namespace WDP_Egzamin
+{
+	class Program
+	{
+		#region Klasy z zadania 4
+		public class Klient
+		{
+			public string nazwisko; // Domyślnie tutaj nie ma nawet public więc nie da się odczytać nazwiska haha
+		}
+
+		public class Węzeł
+		{
+			public Klient k;
+			public Węzeł następny;
+		}
+
+		public static class Lista
+		{
+			public static Węzeł Głowa;
+		}
+
+		#endregion
+
+		static void Main(string[] args)
+		{
+			// Zadanie 1, grupa A
+			Console.WriteLine("Zadanie 1, grupa A");
+			Console.WriteLine(IleLiczb(new int[] { 1, 11, 3, 4, 5, 6 })); // 1
+
+			// Zadanie 2, grupa A
+			Console.WriteLine("Zadanie 2, grupa A");
+			Console.WriteLine(IleWyrazow("ala ma katar", 'a')); // 2
+
+			// Zadanie 3, grupa A
+			Console.WriteLine("Zadanie 3, grupa A");
+			Console.WriteLine(Suma(new double[] { 1, 2, 4, 6, 8, 9 }, 0, 7, 3.14)); // 10
+
+			// Zadanie 4, grupa A (głupie zadanie, nie wiem jak miałoby zostać sprawdzone automatycznie przez runcode, raczej nie powinno być)
+			#region Przygotowywanie listy pod testowanie zdania
+			var k5 = new Klient();
+			k5.nazwisko = "Wokulski";
+
+			var w5 = new Węzeł();
+			w5.k = k5;
+			w5.następny = null;
+
+			var k4 = new Klient();
+			k4.nazwisko = "Rzecki";
+
+			var w4 = new Węzeł();
+			w4.k = k4;
+			w4.następny = w5;
+
+			var k3 = new Klient();
+			k3.nazwisko = "Wokulski";
+
+			var w3 = new Węzeł();
+			w3.k = k3;
+			w3.następny = w4;
+
+			var k2 = new Klient();
+			k2.nazwisko = "Rzecki";
+
+			var w2 = new Węzeł();
+			w2.k = k2;
+			w2.następny = w3;
+
+			var k1 = new Klient();
+			k1.nazwisko = "Starski";
+
+			var w1 = new Węzeł();
+			w1.k = k1;
+			w1.następny = w2;
+
+			Lista.Głowa = w1;
+			#endregion
+			Zapisz("zadanie4");
+
+			Console.ReadKey();
+		}
+
+		#region Zadanie 4, grupa A
+
+		public static void Zapisz(string nazwa)
+		{
+			var writer = new StreamWriter(nazwa + ".txt");
+
+			var obecnyWęzeł = Lista.Głowa;
+
+			while (obecnyWęzeł != null)
+			{
+				writer.WriteLine(obecnyWęzeł.k.nazwisko);
+				obecnyWęzeł = obecnyWęzeł.następny;
+			}
+
+			writer.Close();
+		}
+
+
+		#endregion
+
+		#region Zadanie 3, grupa A
+
+		static double Suma(double[] T, int i, double x, double y)
+		{
+			if (i == T.Length)
+				return 0;
+
+			if (T[i] < x && T[i] > y)
+				return T[i] + Suma(T, i + 1, x, y);
+
+			return Suma(T, i + 1, x, y); // Wow, to zadanie zajęło mi jakąś minutę haha
+		}
+
+		#endregion
+
+		#region Zadanie 2, grupa A
+
+		static int IleWyrazow(string tekst, char x)
+		{
+			int ileZgodnychWyrazow = 0;
+			int ileZnakówWBieżącymWyrazie = 0;
+
+			foreach (var znak in tekst) 
+			{
+				if (!(znak >= 'a' && znak <= 'z') && !(znak >= 'A' && znak <= 'Z'))
+				{
+					ileZnakówWBieżącymWyrazie = 0;
+					continue;
+				}
+
+				if (znak != x)
+					continue;
+
+				ileZnakówWBieżącymWyrazie += 1;
+
+				if (ileZnakówWBieżącymWyrazie == 2)
+					ileZgodnychWyrazow += 1;
+			}
+
+			return ileZgodnychWyrazow;
+		}
+
+		#endregion
+
+		#region Zadanie 1, grupa A
+
+		static bool CzyPierwsza(int liczba)
+		{
+			if (liczba < 2)
+				return false;
+
+			for (int i = 2; i <= Math.Sqrt(liczba); i++)
+			{
+				if (liczba % i == 0)
+					return false;
+			}
+
+			return true;
+		}
+
+		static int IleLiczb(int[] T)
+		{
+			int suma = 0;
+
+			foreach (int liczba in T){
+				suma += liczba;
+			}
+
+			double średnia = (double) suma / T.Length;
+
+			int ile = 0;
+
+			for (int i = 1; i < T.Length-1; i += 2) 
+			{
+				int liczba = T[i];
+
+				if (!CzyPierwsza(liczba))
+					continue;
+
+				if (liczba > średnia * 2)
+					ile += 1;
+			}
+
+			return ile;
+		}
+
+		#endregion
+	}
+}
